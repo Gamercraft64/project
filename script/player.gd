5bf33181a6b6223aef4da9c2ce370d557c9956fb
Gamercraft64 <aryadewanto30@gmail.com>
2025-11-09 15:53:37 +0700

    added player movement script

---
+++ script/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+@export var speed = 500.00
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_movement()
+
+
+func _movement():
+	var motion = Vector2()
+	if Input.is_action_pressed("right"):
+		motion.x += 1
+	if Input.is_action_pressed("left"):
+		motion.x -= 1
+	if Input.is_action_pressed("up"):
+		motion.y -=1
+	if Input.is_action_pressed("down"):
+		motion.y += 1
+	motion = motion.normalized()
+	velocity = motion * speed
+	move_and_slide()
